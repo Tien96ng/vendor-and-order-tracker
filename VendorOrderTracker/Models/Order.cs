a146a4dd8accfc8ab0ee925478b1fbe07c57a10d
tien96ng <tien96ng@ghmail.com>
2021-03-05 20:39:03 -0800

    Create DateTime functionality for getting the date for a new order at that instance.

---
+++ VendorOrderTracker/Models/Order.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System;
+using System.Globalization;
+
+namespace VendorOrderTracker.Models
+{
+  public class Order
+  {
+    // Create an Order class. This class should include properties for the title, the description, the price, the date, and 
+    // any other properties you would like to include.
+    public string Name { get; }
+    public string Description { get; }
+    public string Date { get; }
+    public int Price { get; }
+    public int Id { get; set; }
+    private static List<Order> _instances = new List<Order> {};
+    
+    public Order(string name, string description, int price)
+    {
+      Name = name;
+      Description = description;
+      Price = price;
+      Date = DateTime.Now.ToString();
+    }
+  }
+}
